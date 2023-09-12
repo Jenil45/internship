ce15ad8fcbb4a625967a176ba733428fb7c87e87
Jenil Thakor <jenil.rohit45@gmail.com>
2023-09-12 19:20:48 +0530

    Admin dashboard changes form table responsiveness hamburger

--- Module1/Dashboard Project/js/index.js
+++ Module1/Dashboard Project/js/index.js
@@ -5,28 +5,3 @@ function setSidebar() {
     sidebar.classList.toggle("sidebar-active");
     main.classList.toggle("main-active");
 }
-
-    var dataPoints = [
-    {x:1 , y:100},
-    {x:2 , y:100},
-    {x:3 , y:100},
-    {x:4 , y:100},
-];
-
-var chart = new CanvasJS.Chart("myChart", {
-	animationEnabled: true,
-	theme: "light2",
-	title: {
-		text: "Daily Sales Data"
-	},
-	axisY: {
-		title: "Units",
-		titleFontSize: 24,
-		includeZero: true
-	},
-	data: [{
-        type: "column",
-		yValueFormatString: "#,### Units",
-		dataPoints: dataPoints
-	}]
-});
